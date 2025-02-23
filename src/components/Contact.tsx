46a20d1715c75926088c862b7ca89cd042605fa8
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2025-02-23 16:34:33 -0600

    Fixed text issues in Contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,8 +3,8 @@ import "../styles/Contact.css";
 const Contact = () => {
     return (
         <div id="contact" className="contact">
-            <p className="contactText">email:  luisarguelles2003@gmail.com</p>
-            <p className="contactText">Tel:  +52 86 82 39 92 94</p>
+            <p className="contactText"><a href="mailto:luisarguelles2003@gmail.com">email:  luisarguelles2003@gmail.com</a></p>
+            <p className="contactText">Tel:  +52 868 239 92 94</p>
             <a href="https://www.linkedin.com/in/luis-antonio-arg%C3%BCelles-gonz%C3%A1lez-878a76180/" target="_blank"><img src="linkedin.webp" className="linkedin"/></a>
         </div>
     )
